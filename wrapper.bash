207d424c4033da7a211e0a5286ed1d9d0fe59645
Lightning Auriga <lightning.auriga@gmail.com>
2022-11-18 13:11:30 -0500

    chore: remove force rerun for snakemake dispatch script

--- wrapper.bash
+++ wrapper.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-snakemake -j50 --profile ../sge-profile -p --rerun-incomplete --use-conda -F --rerun-triggers mtime
+snakemake -j50 --profile ../sge-profile -p --rerun-incomplete --use-conda --rerun-triggers mtime
