1183cbe9126cce70da5137b1598250c1d39441fb
Lightning Auriga <lightning.auriga@gmail.com>
2022-11-18 13:08:45 -0500

    chore: adjust rerun triggers for rerunning jobs

--- wrapper.bash
+++ wrapper.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-snakemake -j50 --profile ../sge-profile -p --rerun-incomplete --use-conda -F
+snakemake -j50 --profile ../sge-profile -p --rerun-incomplete --use-conda -F --rerun-triggers mtime
