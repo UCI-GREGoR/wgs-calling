6fdd98c7e8e1161da2b360570e8b450b96663641
Lightning Auriga <lightning.auriga@gmail.com>
2022-11-29 21:03:26 +0000

    fix: dedup subject id list before creating somalier pedfile
    this function was initially developed using single lane test data, and so the duplicate issue only
    appeared once the lanes expanded

--- workflow/scripts/construct_somalier_pedfile.py
+++ workflow/scripts/construct_somalier_pedfile.py
@@ -7,14 +7,18 @@ def run_construct_somalier_pedfile(sampleids: list, outfn: str) -> None:
 
     Currently, this populates the sex entry for all subjects with placeholder 0 for unknown.
     """
+    ## Due to fastqs from multiple lanes, the input sample list may contain duplicates,
+    ## which is not acceptable in this context.
+    sampleids_unique = list(set(sampleids))
+    sampleids_unique.sort()
     x = pd.DataFrame(
         data={
-            "FID": sampleids,
-            "Sample": sampleids,
-            "Pat": ["0" for x in sampleids],
-            "Mat": ["0" for x in sampleids],
-            "Sex": ["0" for x in sampleids],
-            "Pheno": ["-9" for x in sampleids],
+            "FID": sampleids_unique,
+            "Sample": sampleids_unique,
+            "Pat": ["0" for x in sampleids_unique],
+            "Mat": ["0" for x in sampleids_unique],
+            "Sex": ["0" for x in sampleids_unique],
+            "Pheno": ["-9" for x in sampleids_unique],
         }
     )
     x.to_csv(outfn, sep="\t", index=False, header=False)
