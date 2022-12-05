5b0da5292b529c14421cac17be9755744b416745
Lightning Auriga <lightning.auriga@gmail.com>
2022-12-05 16:13:19 +0000

    test: add tests for performance metrics plots

--- workflow/scripts/test-aggregate_performance_metrics.R
+++ workflow/scripts/test-aggregate_performance_metrics.R
@@ -7,22 +7,24 @@ if (!isClass("snakemake.s4")) {
 
 source("aggregate_performance_metrics.R")
 
+demo.data <- data.frame(
+  "s" = sample(1:100, 1),
+  "h:m:s" = paste(sample(1:100, 1), sample(1:100, 1), sample(1:100, 1), sep = ":"),
+  "max_rss" = sample(1:1000, 1),
+  "max_vms" = sample(1:1000, 1),
+  "max_uss" = sample(1:1000, 1),
+  "max_pss" = sample(1:1000, 1),
+  "io_in" = sample(1:1000, 1),
+  "io_out" = sample(1:1000, 1),
+  "mean_load" = sample(1:1000, 1),
+  "cpu_time" = sample(1:1000, 1),
+  check.names = FALSE
+)
+
 create.tmp.metrics.file <- function(parent.dirname, metrics.path) {
   dir.create(file.path(parent.dirname, dirname(metrics.path)), recursive = TRUE)
   out.fn <- file.path(parent.dirname, metrics.path)
-  out.data <- data.frame(
-    "s" = sample(1:100, 1),
-    "h:m:s" = paste(sample(1:100, 1), sample(1:100, 1), sample(1:100, 1), sep = ":"),
-    "max_rss" = sample(1:1000, 1),
-    "max_vms" = sample(1:1000, 1),
-    "max_ums" = sample(1:1000, 1),
-    "max_pss" = sample(1:1000, 1),
-    "io_in" = sample(1:1000, 1),
-    "io_out" = sample(1:1000, 1),
-    "mean_load" = sample(1:1000, 1),
-    "cpu_time" = sample(1:1000, 1),
-    check.names = FALSE
-  )
+  out.data <- demo.data
   write.table(out.data, out.fn, row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
   out.data
 }
@@ -86,4 +88,50 @@ test_that("get.cpu.walltime.description returns a seemingly-relevant character v
 test_that("get.walltime.description returns a seemingly-relevant character vector", {
   output <- get.walltime.description()
   expect_true(stringr::str_detect(output, stringr::regex("^\n\nWalltime represents.*\n\n$", dotall = TRUE)))
+})
+
+## testing ggplot functions is an imperfect art
+test_that("walltime.plot returns a minimally-conformant plot", {
+  output <- walltime.plot(demo.data, "rulename")
+  ## check that standard theme has been applied
+  expect_equal(output$theme$plot.title$hjust, 0.5)
+  ## check that it is geom_point
+  expect_true(!is.null(output$layers))
+  expect_true(inherits(output$layers[[1]]$geom, "GeomPoint"))
+  ## check that it just has the one layer
+  expect_equal(length(output$layers), 1)
+})
+
+test_that("cpu.div.walltime.plot returns a minimally-conformant plot", {
+  output <- cpu.div.walltime.plot(demo.data, "rulename", 2)
+  ## check that standard theme has been applied
+  expect_equal(output$theme$plot.title$hjust, 0.5)
+  ## check that it is geom_point
+  expect_true(!is.null(output$layers))
+  expect_true(inherits(output$layers[[1]]$geom, "GeomPoint"))
+  ## check that it has two layers: the points and the threads line
+  expect_equal(length(output$layers), 2)
+  expect_true(inherits(output$layers[[2]]$geom, "GeomHline"))
+})
+
+test_that("memory.plot returns a minimally-conformant plot", {
+  output <- memory.plot(demo.data)
+  ## check that standard theme has been applied
+  expect_equal(output$theme$plot.title$hjust, 0.5)
+  ## check that it is geom_point
+  expect_true(!is.null(output$layers))
+  expect_true(inherits(output$layers[[1]]$geom, "GeomPoint"))
+  ## check that it just has the one layer
+  expect_equal(length(output$layers), 1)
+})
+
+test_that("io.plot returns a minimally-conformant plot", {
+  output <- io.plot(demo.data)
+  ## check that standard theme has been applied
+  expect_equal(output$theme$plot.title$hjust, 0.5)
+  ## check that it is geom_point
+  expect_true(!is.null(output$layers))
+  expect_true(inherits(output$layers[[1]]$geom, "GeomPoint"))
+  ## check that it just has the one layer
+  expect_equal(length(output$layers), 1)
 })
