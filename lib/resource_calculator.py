1a33418a156f71baf33b6e96946fc30e94bb8eed
Lightning Auriga <lightning.auriga@gmail.com>
2022-12-06 02:51:45 +0000

    fix: unpin most memory requests from octopus and try more cores

--- lib/resource_calculator.py
+++ lib/resource_calculator.py
@@ -3,5 +3,5 @@ def compute_octopus_mem_mb(wildcards, input, threads, attempt) -> int:
     Compute scaling RAM requirements for octopus_run_task with each resubmission
     to the compute controller
     """
-    octopus_scaling_ram = 36000
+    octopus_scaling_ram = 22000
     return int(threads * octopus_scaling_ram * (1 + (attempt - 1) * 0.1))
