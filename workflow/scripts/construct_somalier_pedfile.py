6c39f4a5b4b1d0ed83c4d11d610225b67134f759
Lightning Auriga <lightning.auriga@gmail.com>
2023-01-17 12:01:18 -0500

    feat: extract sex and relatedness data from PMGRC subject IDs
    according to upstream, the PMGRC sample IDs pulled in from the 'logbook' have encoded relatedness
    and sample sex data in them, as PMGRC-(uniqueid)-(probandid)-(0 if proband, 1 if proband father, 2
    if proband mother, 3 if other). this information is now theoretically used to improve the QC metrics
    emitted by somalier after alignment

--- workflow/scripts/construct_somalier_pedfile.py
+++ workflow/scripts/construct_somalier_pedfile.py
@@ -32,21 +32,41 @@ def run_construct_somalier_pedfile(linker: str, ruid: str, sampleids: list, outf
 
     ## iterate across manifest subjects and try to find matching values
     self_reported_sex = []
+    mat_id = []
+    pat_id = []
+    parent_data = {}
+    for sampleid in ids["sampleid"]:
+        parsed_sample_id = sampleid.split("-")
+        parent_data["{}-{}".format(parsed_sample_id[2], parsed_sample_id[3])] = sampleid
+
     for ruid, sampleid in zip(ids["ruid"], ids["sampleid"]):
         sample_sex = linker_data.loc[
             (linker_data["ru"] == ruid) & (linker_data["sq"] == sampleid), "sex"
         ]
+        parsed_sample_id = sampleid.split("-")
         if len(sample_sex) == 1:
             self_reported_sex.append(convert_sex_representation(sample_sex.to_list()[0]))
+        elif parsed_sample_id[3] == "1":
+            self_reported_sex.append(convert_sex_representation("Male"))
+        elif parsed_sample_id[3] == "2":
+            self_reported_sex.append(convert_sex_representation("Female"))
         else:
             self_reported_sex.append(0)
+        if "{}-1".format(parsed_sample_id[1]) in parent_data:
+            pat_id.append(parent_data["{}-1".format(parsed_sample_id[1])])
+        else:
+            pat_id.append("0")
+        if "{}-2".format(parsed_sample_id[1]) in parent_data:
+            mat_id.append(parent_data["{}-2".format(parsed_sample_id[1])])
+        else:
+            mat_id.append("0")
 
     x = pd.DataFrame(
         data={
             "FID": ids["sampleid"],
             "Sample": ids["sampleid"],
-            "Pat": ["0" for x in ids["sampleid"]],
-            "Mat": ["0" for x in ids["sampleid"]],
+            "Pat": pat_id,
+            "Mat": mat_id,
             "Sex": self_reported_sex,
             "Pheno": ["-9" for x in ids["sampleid"]],
         }
