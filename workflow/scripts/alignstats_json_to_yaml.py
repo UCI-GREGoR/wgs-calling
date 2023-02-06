33c3491a0eedd33dc133d961876fd44089f2d1b9
Lightning Auriga <lightning.auriga@gmail.com>
2023-02-06 07:40:11 -0500

    fix: adjust output yaml sort order and purge redundant key/value pair

--- workflow/scripts/alignstats_json_to_yaml.py
+++ workflow/scripts/alignstats_json_to_yaml.py
@@ -25,9 +25,11 @@ def run_alignstats_json_to_yaml(infiles, outfile):
         with open(align_json, "r") as in_j:
             d = json.load(in_j)
             sample_name = d["InputFileName"].split("/")[-1].split(".")[0]
+            ## remove InputFileName, as it does nothing and the table is already huge
+            removed_key = d.pop("InputFileName", None)  # noqa: F841
             out_dict["data"][sample_name] = d
     with open(outfile, "w") as f:
-        yaml.dump(out_dict, f)
+        yaml.dump(out_dict, f, sort_keys=False)
 
 
 run_alignstats_json_to_yaml(snakemake.input["json"], snakemake.output["yaml"])  # noqa: F821
