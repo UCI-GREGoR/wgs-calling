0f23f9d0687f6c7b5dac7dc16f50e8be8ce1cdcb
Lightning Auriga <lightning.auriga@gmail.com>
2023-04-14 13:59:54 -0400

    test: add test for when user queue specification is invalid

--- lib/test_resource_calculator.py
+++ lib/test_resource_calculator.py
@@ -14,5 +14,18 @@ def queue_set():
     [("small", ["q1", "q2", "q3"]), ("large", ["q4", "q5"]), ("huge", ["q6"])],
 )
 def test_select_queue(queue_set, selected_queue, expected_queues):
+    """
+    Test select_queue when the user selection is valid
+    """
     res = [rc.select_queue(selected_queue, queue_set) for i in range(100)]
     assert set(res) == set(expected_queues)
+
+
+def test_select_queue_invalid_selection(queue_set):
+    """
+    Test select_queue when the user selection is invalid
+    """
+    with pytest.raises(
+        ValueError, match=r"Configured queue set does not match anything in user resource config.*"
+    ):
+        rc.select_queue("fake_queue", queue_set)
