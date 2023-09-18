fce31edc6234b371633f6519cd437908ff0b71ee
Lightning Auriga <lightning.auriga@gmail.com>
2023-09-18 13:21:47 -0400

    perf: add conditional preflight conda environment creation to wrapper
    a single pass --conda-create-envs-only will be executed when the wrapper is used in a directory with
    no existing conda environments installed by snakemake. this should hopefully alleviate some sporadic
    issues with remotes that seem to come up when conda creation delays their jobs' execution too long.

--- wrapper.bash
+++ wrapper.bash
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-snakemake -j50 --profile ../sge-profile -p --rerun-incomplete --rerun-triggers mtime --use-conda --use-singularity --cluster-config config/cluster.yaml
+
+## stability update: there are a ton of conda environments in the full run of this workflow.
+## during the initial run of a workflow, the DAG builds, remotes are connected, and then
+## conda environments are linearly constructed, very slowly. this creates situations where
+## the remotes then fail after the environments are constructed. at the same time, constructing
+## the DAG itself can be a costly operation for some runs.
+## as a hack: if no conda environments are present at all, run a preflight pass that
+## exclusively constructs conda environments.
+if [[ ! -d .snakemake/conda ]] ; then
+    snakemake -j1 -p --rerun-incomplete --rerun-triggers mtime --use-conda --use-singularity --conda-create-envs-only
+fi
+snakemake -j150 --profile ../sge-profile -p --rerun-incomplete --rerun-triggers mtime --use-conda --use-singularity --cluster-config config/cluster.yaml
